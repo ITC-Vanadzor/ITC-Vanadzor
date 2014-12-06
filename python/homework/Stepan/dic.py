664dc2fbe8a33f08eb0aebebb714718374f7f97e
Stepan <StepanChaparyan>
2014-12-07 00:19:40 +0400

    add dictionary file

---
+++ python/homework/Stepan/dic.py
@@ -0,0 +1,14 @@
+""" dict. orinak by Stepan
+"""
+#!/usr/bin/python
+
+stepandict = {'Name': 'stepan', 'surname': "chaparyan", 'age' : '33'}
+"""stexcum enq stepandict anunov dictionary """
+
+print "KEYS   _:  ", stepandict.keys();
+"""tpum enq miayn key-ern"""
+print "VALUES _:  ", stepandict.values();
+"""tpum enq miayn value-nern """
+print "ITEMS  _:  ", stepandict.items();
+"""tpum enq item-nern """
+
