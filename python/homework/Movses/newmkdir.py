52770fb4f04d264469e21bf1f31ff9e48363bccf
Movses <xmovses12@mail.ru>
2014-12-08 21:34:36 +0400

    Added homework

---
+++ python/homework/Movses/newmkdir.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p',help="TODO")
+parser.add_argument('path',help="TODO")
+os.path.basename()
+
+if path and args.p:
+    try:
+        makedirs(path)
+    except Exception:
+        print 'error' 
+
+if path:
+    mkdir(path, 0755)
+
+
+
+
+
