13cbf17178da1731f0426a6efa97fa6be7557ba1
Movses <xmovses12@mail.ru>
2014-12-08 22:01:00 +0400

    Added homework

--- python/homework/Movses/newmkdir.py
+++ python/homework/Movses/newmkdir.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-p',help="TODO")
 parser.add_argument('path',help="TODO")
-os.path.basename()
+path = args.path
 
 if path and args.p:
     try:
