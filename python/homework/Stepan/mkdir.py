9b4381a19b3f05288c52f1417173f55dc6b89f62
Mane Hambardzumyan <hambardzumyan.mane@gmail.com>
2014-12-09 18:03:45 +0400

    Added unit test example.

--- python/homework/Stepan/mkdir.py
+++ python/homework/Stepan/mkdir.py
@@ -3,17 +3,64 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument(‘-p’, help=”TODO”)
-parser.add_argument(‘path’, help=”TODO”)
+def my_parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help="TODO")
+    return parser.parse_args()
 
-my_args = parser.parse_args()
+def my_parse_args1():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', required=True,  action='store_true', help="TODO")
+    return parser.parse_args()
 
+def echo(string):
+    print string
 
-#if path and args.p:
-#    try:
-#        makedirs(path)
-#    except Exception:
-#        print ‘error’
-#        if path:
-#            mkdir(path, 0755)
+def echo_name():
+    echo(__name__)
+
+def create_dir_rec(poxos):
+    # TODO check path is dir
+    # TODO check is dir valid
+    # TODO arg
+    
+    karen = os.path.abspath(os.path.join(poxos, os.pardir))
+    my_list = []
+    my_list.append(karen)
+    while(not os.path.exists(karen)):
+        karen = os.path.abspath(os.path.join(karen, os.pardir))
+        my_list.append(karen)
+
+    my_list.pop()
+    my_list.reverse()
+
+    for item in my_list:
+        my_mkdir(item)
+    
+
+def my_mkdir(poxos):
+    # TODO check path is dir
+    # TODO check is dir valid
+    
+    parent = os.path.abspath(os.path.join(poxos, os.pardir))
+    if not os.path.exists(parent):
+        return False
+    try:
+        os.mkdir(poxos)
+        #makedirs(path)
+    except OSError, e:
+        echo("something wrong, Error is " + str(e))
+    return True
+    #except Exception:
+    #print 'error'
+
+if "__main__" == __name__:
+    args = my_parse_args()
+    is_created = my_mkdir(args.path)
+    if not is_created and args.p:
+        create_dir_rec(args.path)
+    elif not is_created and not args.p:
+        my_parse_args1()
+         
+    echo("chem haskanum inch cheq haskanum")
+    echo_name()
