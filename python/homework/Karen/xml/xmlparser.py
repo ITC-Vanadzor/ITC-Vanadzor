eb519836699ef7817eb39c287debbc46d86e5de0
Karen <karkochart@gmail.com>
2014-12-11 05:09:28 +0400

    Xml Homework

---
+++ python/homework/Karen/xml/xmlparser.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+class XmlParser:
+	def parse(self, file):
+		return False
+
+	def is_xml(self, string):
+		return False
+
+	def get_element_count(self):
+		return 0
+
+	def get_xml_structure(self):
+		return '<a><b></b><c></c></a>'
