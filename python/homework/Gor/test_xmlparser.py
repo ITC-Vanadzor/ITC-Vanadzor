fc1387a384e347490dcd98847e80249d4ff6d539
Gor Marine <gorjulhakyan902gmail.com>
2014-12-11 14:59:36 +0400

    Test

--- python/homework/Gor/test_xmlparser.py
+++ python/homework/Gor/test_xmlparser.py
@@ -5,22 +5,22 @@ import os
 class TestXmlParser(unittest.TestCase):
     def test_get_element_count(self):
         xml_parser = XmlParser()
-        self.assertEqual(expected, xml_parser.get_element_count() "Element error")
+        self.assertEqual(self.xml_parser.get_element_count() "Element error")
         assert False
 
     def test_get_xml_structure(self):
         xml_parser = XmlParser()
-        self.assertEqual(expected, xml_parser.get_xml_structure() "XML structure error")
+        self.assertEqual(self.xml_parser.get_xml_structure() "XML structure error")
         assert False
 
     def test_is_xml(self):
         xml_parser = XmlParser()
-        self.assertEqual(expected, xml_parser.is_xml() "String error")
+        self.assertEqual(self.xml_parser.is_xml() "String error")
         self.assert False
 
     def test_parse(self):
         xml_parser = XmlParser()
-        self.assertEqual(expected, xml_parser.parse() "File error")
+        self.assertEqual(self.xml_parser.parse() "File error")
         assert False 
   
   def test_cp3(self):
