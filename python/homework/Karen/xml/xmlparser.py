b7025baa7b65f2d45311772ba8ab86500458695d
Karen <karkochart@gmail.com>
2014-12-14 01:35:05 +0400

    Xml Parser Code

--- python/homework/Karen/xml/xmlparser.py
+++ python/homework/Karen/xml/xmlparser.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+
+import xml.sax
+
 class XmlParser:
 	def parse(self, file):
 		return False
@@ -11,3 +14,35 @@ class XmlParser:
 
 	def get_xml_structure(self):
 		return '<a><b></b><c></c></a>'
+
+class MyXmlContentHandler(xml.sax.ContentHandler):
+    def __init__(self):
+        self.current_data = ""
+        self.name = ""
+        self.age = ""
+
+    def startElement(self, tag, attr):
+        self.current_data = tag
+        if self.current_data == "student":
+            print "----------student---------", self.current_data
+
+    def characters(self, cont):
+        if self.current_data == "name":
+            self.name = cont
+        if self.current_data == "age":
+            self.age = cont
+
+    def endElement(self, tag):
+        if self.current_data == "name":
+            print "Name: ", self.name
+        if self.current_data == "age":
+            print "Age: ", self.age
+        self.current_data = ""
+
+if __name__ == "__main__":
+    parser = xml.sax.make_parser()
+    handler = MyXmlContentHandler()
+    parser.setContentHandler(handler)
+
+    parser.parse("students.xml")
+
