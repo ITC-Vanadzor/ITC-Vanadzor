bd34b354a4cc990b4dfdcba79f3faa0f17b8993f
marine_grigoryan <marishok2890@mail.ru>
2014-12-16 23:16:32 +0400

    homework 15.12.14

--- bash/homework/Marine/bash_new/file_size.sh
+++ bash/homework/Marine/bash_new/file_size.sh
@@ -7,7 +7,7 @@ do
   if [ -f "$file_name" ]
     size_file = `stat -c %s "$i"`
     if [ ${size_file} -gt 1048576 ]; then
-       rm -i "${size_file}"
+       rm -i "${file_name}"
     fi
   fi
 done
