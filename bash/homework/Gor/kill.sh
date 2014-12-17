7f2c6fbdcb7fa57290bfa353de3b1895b11b75e8
Gor <gorjulhakyan90@gmail.com>
2014-12-17 06:01:10 +0400

    .

--- bash/homework/Gor/kill.sh
+++
@@ -1,8 +0,0 @@
-#1/bin/bash
-
-
-PID=`pgrep $1`  
-kill $PID
-
-
-
--- bash/homework/Gor/rn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-path=$(gdialog --title "Path of files" --inputbox "Please insert path of files:" 50 60 2>&1)
-old=$(gdialog --title "Current format" --inputbox "Please insert file format to change:" 50 60 2>&1)
-new=$(gdialog --title "New format" --inputbox "Please insert new file format:" 50 60 2>&1)
-
-cd ${path}
-find . -name "*.$old" -exec rename -v 's/\.'$old'/\.'$new'/i' {} \;
