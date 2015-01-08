f911c41aa4c5167f9f98934a6942d2f95d665c7a
Sargis <sargiskocharyan@yahoo.com>
2015-01-09 02:35:11 +0400

    1 function was added

--- bash/homework/Sargis/Dec_19/spider.sh
+++ bash/homework/Sargis/Dec_19/spider.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Global variables 
+#Global variables
 DECK=52
 CARDS_IN_SUIT=13
 SUIT=4
@@ -13,7 +13,7 @@ declare -a total_deck
 declare -a suit
 declare -a card_number
 declare  cards_state  #picked:1 or unpicked:0
-card_number=(2 3 4 5 6 7 8 9 10 J Q K A)   #cards of one suit
+card_number=(2 3 4 5 6 7 8 9 10 JACK QUEEN KING ACE)   #cards of one suit
 suit=(C D H S)
 cards_state=0
 
@@ -26,9 +26,7 @@ initialize_deck(){
                      deck[i]="${suit[k]} ${card_number[j]}"
                     # echo -e "\t${deck[i]}"
                      let ++i
-                   
               done
-
          done
 }
 initialize_deck
@@ -41,7 +39,7 @@ create_total_deck(){
             j=0
         fi
         total_deck[i]=${deck[j]}
-       # echo "$i    ${total_deck[i]}"
+        #echo "$i    ${total_deck[i]}"
     done
 }
 create_total_deck
@@ -53,12 +51,33 @@ print_table(){
 #print_table
 
 shuffle_total_deck(){
-   shuf_deck=$(shuf -e ${total_deck[@]})
-    #echo "${total_deck[@]}"
-
+    for ((i=$(($TOTAL_DECK-1)); i>0; i--))
+      do  
+        random_number=$((RANDOM%$(($i+1))+0))
+        #echo $random_number
+        a=${total_deck[$i]}
+        total_deck[$i]=${total_deck[$random_number]}
+        total_deck[$random_number]=$a
+      done
 }
 shuffle_total_deck
 for((i=0;i<104;i++))
-do
-    echo "$i  ${shuf_deck[i]}"
-done
+    do
+        echo "$i  ${total_deck[i]}"
+    done
+test_shuffle()
+{
+    as=0
+    for((i=0;i<$TOTAL_DECK;i++))
+        do
+            for((j=$(($i+1));j<$TOTAL_DECK;j++))
+                do 
+                    if [ "${total_deck[$i]}" == "${total_deck[$j]}" ]
+                        then
+                        let ++as
+                        fi
+                 done
+        done
+   echo $as
+}
+test_shuffle
