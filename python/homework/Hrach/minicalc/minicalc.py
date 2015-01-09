23d85b3e80c0186bbafd4d1e002293d347ebe7d3
Hrach <Hrach.M.96@mail.ru>
2015-01-09 18:25:35 +0400

    fix crashes with an error, Preparing to write comments in the style of the Sphinx

--- python/homework/Hrach/minicalc/minicalc.py
+++ python/homework/Hrach/minicalc/minicalc.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+"""
+Calculator
+==========
+"""
+
+
+
 A1 = 'a'
 A2 = 'a'
 A3 = 'a'
@@ -8,20 +15,26 @@ if '__main__' == __name__:
 		if isinstance( A1, int ) or isinstance( A1, float ):
 			break
 		else:
-			A1 = input( 'input number1: ' )
+			try:
+				A1 = input( 'input number1: ' )
+			except NameError:
+				print "it's not number"
 	while True:
 		if A2 != '+' and A2 != '-' and A2 != '*' and A2 !='/':
 			A2 = raw_input( 'input + or - or * or / : ' )
 		else: 
 			break
 	while True:
-		if A3 == 0:
-			print "integer division or modulo by zero"
+		if A2 == '/' and A3 == 0:
+			print "division by zero!"
 			A3 = input( 'input number2: ' )
 		elif isinstance( A3, int ) or isinstance( A3, float ):
 			break
 		else:
-			A3 = input( 'input number2: ' )
+			try:
+				A3 = input( 'input number2: ' )
+			except NameError:
+				print "its not nuber"
 
 def gum():
 	global A
