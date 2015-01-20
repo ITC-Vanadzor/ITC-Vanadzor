434ce707c8ed7fa8c6c10a13ec87f8077e696c17
Karen <karen@gmail.com>
2015-01-20 17:58:11 +0400

    Add work

---
+++ JS/work/Karen_Stepan/regexp/js/functions.js
@@ -0,0 +1,6 @@
+function myFunction() {
+            var str = "Visit W3Schools!"; 
+                        var n = str.search(/w3Schools/g);
+                                        document.getElementById("demo").innerHTML = n;
+}
+
