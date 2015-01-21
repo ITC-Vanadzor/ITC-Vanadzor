32782665bd0797a90e280a454688eb638fbecb76
Edvard-Vahan <youu@exxample.com>
2015-01-21 18:03:37 +0400

    added unfinished calc work

---
+++ JS/homework/Vahan/counter/counter.js
@@ -0,0 +1,12 @@
+function sum(){
+    var x = document.getElementById("leftOp").value;
+    var y = document.getElementById("rightOp").value;
+    var result = x + y;
+    document.getElementById("demo").innerHTML = result;
+}
+
+function equal(){
+    var x = getElementById("leftOp").value;
+    var y = getElementById("rightOp").value;
+    
+}
