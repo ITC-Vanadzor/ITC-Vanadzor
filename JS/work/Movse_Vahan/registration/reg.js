90dbb9c087c05869a1efcfbebc223828745199ce
Edvard-Vahan <youu@exxample.com>
2015-01-22 18:03:20 +0400

    Registration added Movses-Vahan

--- JS/work/Movse_Vahan/registration/reg.js
+++ JS/work/Movse_Vahan/registration/reg.js
@@ -1,14 +1,20 @@
-function isValid() {
-    var patt1 = /\W/g;
-    var myName = document.getElementById("name").value;
-    var result = myName.match(patt1);
-    console.log(result);
+function validateName() {
+    var x = document.forms["myForm"]["fname"].value;
+    var y = document.forms["myForm"]["lname"].value;
+    var patt = /\W/g;
+    var resultN = x.match(patt);
+    var resultL = y.match(patt);
+    if ( (x == null || x == "") || (resultN != null) ) {
+        alert("First name must be filled out correctly!");
+        return false;
+    }
+    else if ( (y == null || y == "") || (resultL != null) ) {  
+        alert("Last name must be filled out correctly!");
+        return false;
+    }
 }
-/*if (myName =  ) {
-    
-}*/
 
-/*function validateForm() {
+function validateMail() {
     var x = document.forms["myForm"]["email"].value;
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
@@ -16,4 +22,21 @@ function isValid() {
         alert("Not a valid e-mail address");
         return false;
     }
-}*/
+}
+
+function validatePassword {
+    var x = document.form["myForm"]["pswd"].value;
+    var patt = /\W\d\w/g;
+    var result = x.match(patt)
+    if ((x.length < 6) || (result == null) || (x == null || x == ""))
+        alert("Password is not valid");
+        return false;
+}
+
+function isValid() {
+    validateName();
+    validateMail();
+    validatePassword();
+}
+
+
