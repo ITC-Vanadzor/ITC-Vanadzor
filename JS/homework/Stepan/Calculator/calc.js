a0ea5b9fbcb4f97a6290a9574e13d43d4136ea27
Stepan <StepanChaparyan>
2015-01-27 22:41:13 +0400

    update calc.js file, insert alert line for string

--- JS/homework/Stepan/Calculator/calc.js
+++ JS/homework/Stepan/Calculator/calc.js
@@ -1,27 +1,43 @@
 function calc(form) {
+  var result = "0";
+  var num1 = document.getElementById("number_1").value;
+  var operator = document.getElementById("operator").value;
+  var num2 = document.getElementById("number_2").value;
 
-var D = "0";
-var A = document.getElementById("num1").value;
-var B = document.getElementById("operator").value;
-var C = document.getElementById("num2").value;
+   if (operator == "+"){
+    result = add(num1, num2); 
+   } else if (operator == "-") {
+    result = minus(num1, num2); 
+   } else if (operator == "*") {
+    result = multiple(num1, num2); 
+   } else if (operator == "/") {
+    result = division(num1, num2); 
+   } else {
+    alert(" Plesae insert +  or  -  or  *  or  /   ");
+   }
 
-if (B == "+")
-{
-D = parseInt(A) + parseInt(C); 
+    document.getElementById("result").innerHTML = result;
+    return false;
 }
-else if(B == "-")
-{
-D = parseInt(A)-parseInt(C);
+
+function add(number_1, number_2){
+  var res = parseInt(number_1) + parseInt(number_2);
+  return res;
 }
-else if(B == "*")
-{
-D = parseInt(A)*parseInt(C);
+
+function minus(number_1, number_2){
+  var res = parseInt(number_1) - parseInt(number_2);
+  return res;
 }
-else if (B == "/")
-{
-D = parseInt(A)/parseInt(C);
+
+function multiple(number_1, number_2){
+  var res = parseInt(number_1) * parseInt(number_2);
+  return res;
 }
-document.getElementById("result").innerHTML = D;
-return false;
+
+function division(number_1, number_2){
+  var res = parseInt(number_1) / parseInt(number_2);
+  return res;
 }
+
 
