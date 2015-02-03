8d0cac41971cdbcf0bb7f0eab7c93c6f49d49c70
Hrach <Hrach.M.96@mail.ru>
2015-02-04 02:39:09 +0400

    added homeworks

---
+++ php/homework/Hrach/table.php
@@ -0,0 +1,23 @@
+<?php
+	$rows=4;
+	$cols=5;
+?>
+
+<?php
+	echo "<!DOCTYPE html>";
+	echo "<html>";
+	echo "<head>";
+	echo "<title>$a</title>";
+	echo "</head>";
+	echo "<body>";
+	echo "<table border=\"1\">";
+		for ($r =0; $r < $rows; $r++){
+			echo'<tr>';
+			for ($c = 0; $c < $cols; $c++)
+					echo '<td>' .a. '</td>';
+			echo '</tr>';
+		}
+	echo"</table>";
+	echo "</body>";
+	echo "</html>";
+?>
---
+++ php/homework/Hrach/var_in_html.php
@@ -0,0 +1,16 @@
+<?php
+$a="its default title";
+$b="text";
+?>
+
+<?php
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<title>$a</title>";
+echo "</head>";
+echo "<body>";
+echo "<span style=\"color:red; font-weight:bold\">$b</span>";
+echo "</body>";
+echo "</html>";
+?>
