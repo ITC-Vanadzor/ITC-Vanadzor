b058dd2da6fe65f572418b59128f541bbde22461
Hrach, Movses <Hrach.M.96@mail.ru, xmovses12@mail.ru>
2015-02-04 18:01:33 +0400

    Update

---
+++ php/homework/Hrach/multip.php
@@ -0,0 +1,15 @@
+<?php
+    $n=5;
+	echo "<!DOCTYPE html>";
+	echo "<html>";
+	echo "<head>";
+	echo "<title>$a</title>";
+	echo "</head>";
+	echo "<body>";
+    for ($a =1; $a < 11; $a++){
+        $c=$a*$n;
+        echo "$n*$a=$c","<br>";
+		}
+	echo "</body>";
+	echo "</html>";
+?>
