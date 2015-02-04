876aaeda2ea34bfe9899dfaca1876d871fb5f7c6
Movses <xmovses12@mail.ru>
2015-02-05 01:30:44 +0400

    Author: Movses     Modified and added new functions

--- php/homework/Movses/multi/multi.php
+++ php/homework/Movses/multi/multi.php
@@ -1,12 +1,15 @@
 <?php
-    
-    $num1=2;
-    $num2=1;
-
-    while($num2<11) { 
-        echo $num1 . " " . "X" . " " . $num2 . " " . "=";
-        echo " " . $num2 * $num1 . "\n";
-        $num2++;
+function mult() {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $num1 = $_REQUEST['txt']; 
+        return $num1;
+        echo $num1;
+    }
+   // $num1=141341;
+    for($i=1; $i<11; $i++) { 
+        echo $num1 . " " . "X" . " " . $i . " " . "=";
+        echo " " . $i * $num1 . "\n";
         } 
-
+}
+mult()
 ?>
