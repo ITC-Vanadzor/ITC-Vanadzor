320f64b7711ffbecea464daa05af8eab21b3fa1f
Movses <xmovses12@mail.ru>
2015-02-04 21:10:06 +0400

    Author: Movses     -Added multiplication homework

---
+++ php/homework/Movses/multi/multi.php
@@ -0,0 +1,12 @@
+<?php
+    
+    $num1=2;
+    $num2=1;
+
+    while($num2<11) { 
+        echo $num1 . " " . "X" . " " . $num2 . " " . "=";
+        echo " " . $num2 * $num1 . "\n";
+        $num2++;
+        } 
+
+?>
