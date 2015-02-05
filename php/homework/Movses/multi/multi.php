87607f9cdde1878a2f37006a50182758dbf5fc3a
Movses <xmovses12@mail.ru>
2015-02-05 23:21:12 +0400

    Author: Movses     Modified index.html file and added new style

--- php/homework/Movses/multi/multi.php
+++ php/homework/Movses/multi/multi.php
@@ -1,12 +1,12 @@
+<center>
+
 <?php
 $num1=$_POST['txt'];
 
-//function mult() {
-  // $num1=141341;
-    for($i=1; $i<=10; $i++) { 
-        echo $num1 . " " . "X" . " " . $i . " " . "=";
-        echo " " . $i * $num1 . "<br>";
-        } 
-//}
-mult()
+for($i=1; $i<=10; $i++) { 
+    echo $num1 . " " . "X" . " " . $i . " " . "=";
+    echo " " . $i * $num1 . "<br>";
+}
+
 ?>
+</center>
