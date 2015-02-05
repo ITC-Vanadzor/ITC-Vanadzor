cd21322e280a0c165fa182bbf4c813d6ba651ad3
Hrach, Movses <Hrach.M.96@mail.ru, xmovses12@mail.ru>
2015-02-05 14:31:56 +0400

    Author: Movses     Added not finished version

--- php/homework/Movses/multi/multi.php
+++ php/homework/Movses/multi/multi.php
@@ -1,15 +1,12 @@
 <?php
-function mult() {
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $num1 = $_REQUEST['txt']; 
-        return $num1;
-        echo $num1;
-    }
-   // $num1=141341;
-    for($i=1; $i<11; $i++) { 
+$num1=$_POST['txt'];
+
+//function mult() {
+  // $num1=141341;
+    for($i=1; $i<=10; $i++) { 
         echo $num1 . " " . "X" . " " . $i . " " . "=";
-        echo " " . $i * $num1 . "\n";
+        echo " " . $i * $num1 . "<br>";
         } 
-}
+//}
 mult()
 ?>
