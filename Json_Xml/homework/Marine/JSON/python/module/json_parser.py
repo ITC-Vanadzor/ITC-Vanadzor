6153938c6fa4e761bfe4d16656d72530c56a4da5
marine_grigoryan <marishok2890@mail.ru>
2015-02-11 20:18:16 +0400

    Update json files
    update json_parser.py and add test_json_parser.py

--- Json_Xml/homework/Marine/JSON/python/module/json_parser.py
+++ Json_Xml/homework/Marine/JSON/python/module/json_parser.py
@@ -8,7 +8,7 @@ class Parser:
         self.json_file = json_file
         self.key = key
     
-    def Fineder_key_in_json(self):
+    def Json_Parser(self):
         try:
             json_data=open(self.json_file)
             data = json.load(json_data)
@@ -16,39 +16,39 @@ class Parser:
            print "incorrect file"
            return None 
        	json_key=self.key.split(".")
-	length_json_key=len(json_key)
-	c=json_key[length_json_key-1]
-	k=c.split(':')
-	json_key[length_json_key-1]=k[0]
-
-	if self.key=="":
-	    print "Input json keys"
-	else:
+		length_json_key=len(json_key)
+		c=json_key[length_json_key-1]
+		k=c.split(':')
+		json_key[length_json_key-1]=k[0]
+		if self.key=="":
+		    print "Input json keys"
+		else:
       	    b=data
             try:		
                 for element in json_key:
-   	            b =b[element]
-	    except KeyError:
-		print  element,"undefined"
-		return None
-	    k[1]=int(k[1])	
-          	
-	    if isinstance(b, dict):	
-	        print "Input full path"
-	    else:
-		if k[1]=="":
-		    print b
-		else:
-		    l=0
-		    m=len(b)
-		    if m>k[1]:
-		        for value in b:
-			    if l==k[1]:	
-		     	        print b[l]
-			        break
-			    l=l+1
-		    else:
-			print "Element with this number does not exist"
+   	    	        b =b[element]
+	    	except KeyError:
+				print  element,"   undefined"
+				return None
+	    	k[1]=int(k[1])	
+       	    if isinstance(b, dict):	
+			    print "Input full path"
+	    	else:
+				if k[1]=="":
+		    		print b
+				return b
+				else:
+		    		l=0
+		    		m=len(b)
+		    		if m>k[1]:
+		        		for value in b:
+			    			if l==k[1]:	
+		     	        		print b[l]
+						return b[l]
+			        			break
+			    			l=l+1
+		    		else:
+						print "Element with this number does not exist"
 def parse_command_line_arguments():
     arg_parser = argparse.ArgumentParser(
         description='Command line arguments.')
@@ -61,6 +61,6 @@ if __name__ == "__main__":
     file_name = arguments.file_name
     key_name=raw_input("Input what you need: ")
     obj_parser = Parser(file_name, key_name)
-    obj_parser.Fineder_key_in_json()
+    obj_parser.Json_Parser()
 
 
