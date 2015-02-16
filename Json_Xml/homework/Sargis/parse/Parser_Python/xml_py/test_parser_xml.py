fcd54cca76678b1c686ef9361638d7286bef0488
Sargis <sargiskocharyan@yahoo.com>
2015-02-16 05:18:23 +0400

    xml_parser's python version
    class with methods was added,  file verification, other improvements

---
+++ Json_Xml/homework/Sargis/parse/Parser_Python/xml_py/test_parser_xml.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import unittest
+import xml.etree.ElementTree as ET
+import lxml.etree as etree
+import sys
+
+class Positivetestxmlparser(unittest.TestCase):
+    def 
+
+
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(Positivetestxmlparser)
+unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+
