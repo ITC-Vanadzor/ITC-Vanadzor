fa71dc04cb317fe4d715d45aec8a6189d1dd9d64
Sargis <sargiskocharyan@yahoo.com>
2015-02-17 13:31:01 +0400

    Author: Sargis
    syntax error was fixed in set_cookie function

--- Itc-users/backend/php/setcookie/set_cookie.php
+++ Itc-users/backend/php/setcookie/set_cookie.php
@@ -13,19 +13,24 @@
     <body>
     <div id="output"> 
     <?php
-    /*
+    
     function print_cookie(){
+        global $cookie_name;
+	global $cookie_value;
         if(!isset($_COOKIE[$cookie_name])) {
-                echo $_COOKIE; 
+                echo "Cookie named '" . $cookie_name . "'is not set!"; 
         } else {
                 echo "Cookie '".$cookie_name."' is set!<br>";
                     echo "Value is: ".$_COOKIE[$cookie_name];
         }
     }
-     */
-    echo $_COOKIE['some_user'];
     ?>
     </div>
-    <button id="button" onclick="document.write('<?php print_cookie() ?>')"> Set Cookie </button>
+    <script>
+    function print_cook(){
+	document.getElementById("output").innerHTML = "<?php print_cookie(); ?>";
+    }
+    </script>
+    <button id="button" onclick="print_cook()"> Set Cookie </button>
     </body>
 </html>
