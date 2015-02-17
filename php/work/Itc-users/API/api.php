d42b9a31e4fe7b59d9346cddf15b993f8fbb8ebf
stepan_sargis <stepan@sargis.com>
2015-02-17 18:03:39 +0400

    Updating api.php file

--- php/work/Itc-users/API/api.php
+++ php/work/Itc-users/API/api.php
@@ -1,27 +1,61 @@
-<? php 
+<?php 
 
-$host = '127.0.0.1';
+
+class Database {
+
+    private $db_name;
+    private $host;
+    private $port;
+    private $user;
+    private $password;
+
+       public  function __construct($db_name, $host, $port, $user, $password) {
+        $this->$db_name = $db_name;
+        $this->$host = $host;
+        $this->$port = $port;
+        $this->$user = $user;
+        $this->$password = $password;
+
+        $this->connect();
+    }
+
+    private function connect() {
+        $connection = pg_connect('host =' .host, 'port =' .port, 'datebase =' .db_name, 'user =' .user, 'password =' .password);
+    }
+
+    function addUser () {
+
+    }
+
+    function deleteUser () {
+
+    }
+
+    function updateUser () {
+
+    }
+
+    function checkUser () {
+
+    }
+
+    function getAllUsersData () {
+
+    }
+}
+
+
+$db_name = 'itcusers';
 $port = '5432';
-$datebase = 'itcusers';
-$user = 'pstgres';
+$host = '127.0.0.1';
+$user = 'postgres';
 $password = 'student2014';
 
-$connection = pg_connect('host =' .host, 'port =' .port, 'datebase =' .datebase, 'user =' .user, 'pasword =' .password);
-
-//  INSERT TO THE DATEBASE //
-
-$insert = $connection(INSERT INTO user_personal(NAME, SURNAME, EMAIL, PASSWORD) ) VALUES ('ANUN', 'AZGANUN', 'EMAIL', 'PASSWORD') )
-
-    //  CHEK USER //
-
-$query = "SELECT * FROM user_personal"
-
-// Delete //
+$db_obj = new Database($db_name, $host, $port, $user, $password);
 
 
-
-
-
-
-
+echo "sd"       
+   
 ?>
+
+
