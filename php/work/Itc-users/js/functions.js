8ea29277ec83f072cc9478da8c95e3ce217a2ab2
Karen <karen@gmail.com>
2015-02-17 18:10:43 +0400

    Add json parser in js

--- php/work/Itc-users/js/functions.js
+++ php/work/Itc-users/js/functions.js
@@ -1,47 +1,83 @@
 $(document).ready(function () {
     sellectAllOrNull();
     getEditUser();
+    printUsers();
 });
 //checked first checkbox: all select box in begin line make selected,
 //and remove checked in first checkbox: all make no selected 
-function sellectAllOrNull(){
+function sellectAllOrNull() {
     $('.tablePage .title li input[name="selectAll"]').click(function () {
         if (this.checked) {
             $('.tablePage ul li .checkUser').each(function () {
-                    this.checked=true;
-            });  
-        }else{
+                this.checked = true;
+            });
+        } else {
             $('.tablePage ul li .checkUser').each(function () {
-                    this.checked=false;
+                this.checked = false;
             });
         }
-    });    
+    });
 }
 
-function getEditUser(){
+function getEditUser() {
     var value;
     var user_id;
     var rel;
-    $('.action a').click(function(){
+    $('.action a').click(function () {
         $('.modal-content').html('<ul class="editUser"></ul>');
-        user_id = $(this).parents('.user').find('li[rel="user_id"]').text();
-        $('.editUser').append('<li style="display:none;"><input name="user_id" type="text" value="'+user_id+'" /></li>');
-        $(this).parents('.user').find('div.text').each(function(){
-            rel=$(this).parent().attr('rel');
+        user_id = $(this).parents('.user').find('input[type="checkbox"]').val();
+        $('.editUser').append('<li style="display:none;"><input name="user_id" type="text" class="text" value="' + user_id + '" /></li>');
+        $(this).parents('.user').find('div.text').each(function () {
+            rel = $(this).parent().attr('class');
             value = $(this).text();
-            $('.editUser').append('<li><input type="text" class="text" value="'+value+'" name="'+rel+'"/></li>');
+            $('.editUser').append('<li><input type="text" class="text" value="' + value + '" name="' + rel + '"/></li>');
         });
-        $('.modal-content').append('<button class="updateUser">Update</button>');
-        updateUser();
+        $('.modal-content').append('<button id="updateUser" onclick="getJson()">Update</button>');
+        //      updateUser();
     });
 }
-function updateUser(){
-    $('button.updateUser').click(function(){
-        updateArr=new Array();
-        var user_id = $('.editUser li input[name="user_id"]').val()+"";
-        $('.editUser li input.text').each(function(){
-            updateArr[$(this).attr('name')]=$(this).val();
-        });
-        alert(updateArr.name);
-    });
+function updateUser(json) {
+    //TODO
+}
+function getJson() {
+    var modalHtml = document.getElementById("modal-content");
+    var list = modalHtml.getElementsByClassName("text");
+    var i;
+    var key;
+    var val;
+    var json = {};
+    for (i = 0; i < list.length; i++) {
+        val = list[i].value;
+        key = list[i].getAttribute("name");
+        json[key] = val;
+    }
+    updateTable(json);
+    return json;
+}
+
+function updateTable(json) {
+    var userId = json['user_id'];
+    var table = document.getElementById("tablePage");
+    var list = table.getElementsByTagName("li");
+}
+function printUsers() {
+    objUsers = {"users": [{"userId": "1", "name": "Poghos", "lastname": "Poghosyan", "email": "poghosyan@gmail.com", "sex": "male", "age": "54"},
+            {"userId": "1", "name": "Poghos", "lastname": "Poghosyan", "email": "poghosyan@gmail.com", "sex": "male", "age": "54"},
+            {"userId": "1", "name": "Poghos", "lastname": "Poghosyan", "email": "poghosyan@gmail.com", "sex": "male", "age": "54"}]};
+    //alert(objUsers.users.length);
+    for (i = 0; i < objUsers.users.length; i++) {
+    var parentObj = document.getElementById("tablePage");
+        ul = createElement(parentObj,"ul",{"class":"user"});
+        li = createElement(ul,"li",{});         
+        div = createElement(li,"div",{}); 
+        input = createElement(div,"input",{"type":"checkbox","name":"user","value":"1","class":"checkUser"});                 
+    }
+}
+function createElement(parentObj,tagName,attributes){
+    var tag = document.createElement(tagName);
+        for (var key in attributes) {        
+            tag.setAttribute(key, attributes[key]);
+        }
+    parentObj.appendChild(tag);        
+    return tag;
 }
