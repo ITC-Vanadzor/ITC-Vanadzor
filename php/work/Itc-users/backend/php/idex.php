bbdf5f56a9b747debb6e74168793a3afcd637509
Sargis <sargiskocharyan@yahoo.com>
2015-02-18 12:53:43 +0400

    server API(some changes were made)

--- php/work/Itc-users/backend/php/idex.php
+++ php/work/Itc-users/backend/php/idex.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
 <html>
 <body>
+<?php 
+function get_user(){
+    $curl=curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://get_user.php',
+        CURLOPT_POST => 1
+        )
+    $res = curl_exec($curl);
+    curl_close($curl);
+}
+
+?>
 <script>
-    
 function send_request_POST(){
 	<?php get_user() ?>;
-}   
-function get_user(){
-    curl = curl_init()
  }
-?>
+</script>
 <form method="POST" action="send_request_POST()" id="php_form">
 <input type="submit" value="send POST"> 
 </form>
-<form method="DELETE" action="<?php  delete_user() ?>" id="php_form2" >
+<form method="DELETE" action="send_request_DELETE()" id="php_form2" >
 <input type="submit" value="Send DELETE">
 </form>
 
