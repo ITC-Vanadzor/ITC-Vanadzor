4ff287f38edebd33cccf3d70befc788914661179
Karen <karkochart@gmail.com>
2015-02-25 12:59:57 +0400

    Add homework php login validation form

---
+++ php/homework/Karen/login_php/header.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/style.css"></link>
+        <script src="js/functions.js"></script>
+    </head>
+    <body onload="addSelectYears()">
+        <div class="wrapper">
---
+++ php/homework/Karen/login_php/request.php
@@ -0,0 +1,4 @@
+<?php
+    include 'functions.php';
+    header("Location: index.php?errname=1&errname=1&errlastname=1&erremail=1&errpassword=1&");  
+?>
