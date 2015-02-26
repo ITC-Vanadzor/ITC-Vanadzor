6f07413e29740bfdce01d89bd9ab3085c46842a7
Hrach, Movses <Hrach.M.96@mail.ru, xmovses12@mail.ru>
2015-02-26 17:47:06 +0400

    database api
    modified

--- php/work/Stepan_Sargis/web_php_db/database/database_api.php
+++ php/work/Stepan_Sargis/web_php_db/database/database_api.php
@@ -7,62 +7,58 @@ $db_name = "my_web_db";
 
 // 1. functions
 // 2. createTable($tableName, $fields)
+$userQuery = "CREATE TABLE User_table (
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY , 
+    user VARCHAR(30) , 
+    password VARCHAR(30) )";
+$appQuery = " CREATE TABLE App_table (
+    app_id INT(6) UNSIGNED AUTO_INCREMENT NOT NULL, 
+    app_name VARCHAR(30) NOT NULL)";
+$hisQuery = "create table  History (
+    FOREIGN KEY (user_id) REFERENCES User_table(user_id),
+    FOREIGN KEY (app_id) REFERENCES APP_table(app_id),
+    OrderDate datetime NOT NULL DEFAULT NOW() )";
 
+function createTable($conn,$db_name, $queryS){
+    echo "------NN\n";
+    $prep = "use $db_name";
+    $conn->query($prep);
+    if ($conn->query($queryS) === TRUE) {
+        echo "Table was created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+}
 function create_if_dont_exist($servername, $username, $password, $db_name){
     // Create connection
     $conn = new mysqli($servername, $username, $password, $db_name);
     // Check connection
     if ($conn->connect_error) {
-echo " 111_____iiiii\n";
         $conn = new mysqli($servername, $username, $password);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         } 
-echo " 211_____iiiii\n";
         // Create database
         $sql = "CREATE DATABASE $db_name";
         if ($conn->query($sql) === TRUE) {
-echo " 411_____iiiii\n";
             echo "Database created successfully";
-            $sql = "CREATE TABLE User_table (
-                user_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-                user VARCHAR(30) NOT NULL,
-                password VARCHAR(30) NOT NULL )";
-            if ($conn->query($sql) === TRUE) {
-                    echo "User_table was created successfully";
-            } else {
-                    echo "Error creating User_table: " . $conn->error;
-            }
-            $sql = "CREATE TABLE App_table (
-                app_id INT(6) UNSIGNED AUTO_INCREMENT NOT NULL, 
-                app_name VARCHAR(30) NOT NULLi)";
-            if ($conn->query($sql) === TRUE) {
-                    echo "App_table was created successfully";
-            } else {
-                    echo "Error creating App_table: " . $conn->error;
-            }
-            $sql = "CREATE TABLE History (
-                FOREIGN KEY (user_id) REFERENCES User_table(user_id),
-                FOREIGN KEY (app_id) REFERENCES APP_table(app_id),
-                OrderDate datetime NOT NULL DEFAULT NOW() )";
-            if ($conn->query($sql) === TRUE) {
-                    echo "History table was created successfully";
-            } else {
-                    echo "Error creating History table: " . $conn->error;
-            }
-
+            echo "------NN\n";
+            global $userQuery,$appQuery, $hisQuery ;
+            createTable($conn,$db_name,$userQuery);
+            createTable($conn,$db_name,$appQuery);           
+            createTable($conn,$db_name, $hisQuery);           
         } else {
-echo " 511_____iiiii\n";
             die("Error creating database: " . $conn->error);
         }
+    } else{
+     //  createTable($conn,$userQuery);           
+     //  createTable($conn,$appQuery);           
+     //  createTable($conn,$hisQuery);           
     }
-echo "end 811_____iiiii\n";
     $conn->close();
 }
 
-echo "iiiii\n";
 create_if_dont_exist($servername, $username, $password, $db_name);
-echo "iiii\n";
 
 
 ?>
