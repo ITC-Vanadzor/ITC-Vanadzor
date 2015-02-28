27b4b4737064f8fb141843c33ebbdcc88a9ca08e
Gor <gorjulhakyan90@gmail.com>
2015-03-01 01:40:49 +0400

    Adding SQL homework pc_shop.sql file

--- sql/homework/Gor/pc_shop.sql
+++ sql/homework/Gor/pc_shop.sql
@@ -1,60 +1,108 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: pc_shop
+-- ------------------------------------------------------
+-- Server version	5.5.41-0ubuntu0.14.10.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `categories`
+--
 
-DROP TABLE IF EXISTS `pc`;
-CREATE TABLE IF NOT EXISTS `pc` (
-  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `M_Board` int(10) NOT NULL,
-  `CPU` int(10) NOT NULL,
-  `RAM` int(10) NOT NULL,
-  `GPU` int(10) NOT NULL,
-  `HDD` int(10) NOT NULL,
-  `Cooler` int(10) NOT NULL,
-  `Case` int(10) NOT NULL,
-  `Monitor` int(10) NOT NULL,
-  `Keyboard` int(10) NOT NULL,
-  `Mouse` int(10) NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `ID` (`ID`),
-  UNIQUE KEY `ID_2` (`ID`),
-  UNIQUE KEY `ID_3` (`ID`),
-  UNIQUE KEY `ID_4` (`ID`),
-  KEY `ID_5` (`ID`),
-  KEY `ID_6` (`ID`),
-  KEY `M_Board` (`M_Board`),
-  KEY `M_Board_2` (`M_Board`),
-  KEY `CPU` (`CPU`),
-  KEY `CPU_2` (`CPU`),
-  KEY `RAM` (`RAM`),
-  KEY `Mouse` (`Mouse`),
-  KEY `Mouse_2` (`Mouse`),
-  KEY `Keyboard` (`Keyboard`),
-  KEY `Monitor` (`Monitor`),
-  KEY `Case` (`Case`),
-  KEY `HDD` (`HDD`),
-  KEY `GPU` (`GPU`),
-  KEY `Cooler` (`Cooler`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `category_id` varchar(5) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `categories`
+--
 
-ALTER TABLE `pc`
-  ADD CONSTRAINT `pc_ibfk_11` FOREIGN KEY (`Cooler`) REFERENCES `cooler` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_10` FOREIGN KEY (`Mouse`) REFERENCES `mouse` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_2` FOREIGN KEY (`M_Board`) REFERENCES `mb` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_3` FOREIGN KEY (`CPU`) REFERENCES `cpu` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_4` FOREIGN KEY (`RAM`) REFERENCES `ram` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_5` FOREIGN KEY (`GPU`) REFERENCES `gpu` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_6` FOREIGN KEY (`HDD`) REFERENCES `hdd` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_7` FOREIGN KEY (`Case`) REFERENCES `case` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_8` FOREIGN KEY (`Monitor`) REFERENCES `monitor` (`ID`),
-  ADD CONSTRAINT `pc_ibfk_9` FOREIGN KEY (`Keyboard`) REFERENCES `keybord` (`ID`);
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Processor','CPU'),(2,'Mother Board','MB'),(3,'Random Access Memory','RAM'),(4,'Video Card','GPU'),(5,'Hard Disk Drive','HDD'),(6,'Monitor','MON'),(7,'Keyboard','KB'),(8,'Mouse','MS'),(9,'Case','CASE');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `category` varchar(5) DEFAULT NULL,
+  `product` varchar(50) DEFAULT NULL,
+  `manufacturer` varchar(50) DEFAULT NULL,
+  `price` decimal(5,2) DEFAULT NULL,
+  `total_price` decimal(8,2) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `manufacturer` varchar(50) DEFAULT NULL,
+  `model` varchar(25) DEFAULT NULL,
+  `description` text,
+  `price` decimal(5,2) DEFAULT NULL,
+  `category` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-03-01  1:35:53
