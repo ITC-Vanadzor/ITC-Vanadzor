913334610faccf88add229590a3f45e9d43bc237
marine_grigoryan <marishok2890@mail.ru>
2015-03-02 00:15:22 +0400

    Update php_mysql homework

--- php/homework/Marine/php_web_drive/controller/add_user.php
+++ php/homework/Marine/php_web_drive/controller/add_user.php
@@ -7,17 +7,33 @@ $conn = mysqli_connect($servername, $username, $passwd, $dbname);
 if (!$conn) {
     die('Could cot connect: ' . mysql_error());
 }
-// GET
 echo 'Connected successfully'."\n";
 $user=$_POST['username'];
 $pass=$_POST['pass'];
-$sql = "INSERT INTO Users (name, password)
-VALUES ('$user','$pass')";
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+$appname=$_GET['appname'];
+if ($_SERVER['REQUEST_METHOD']=='POST') { 
+	if (!is_null($_POST['appname'])) {
+		echo "APP name send by Get method";
+	} else{
+		$sql = "INSERT INTO Users (name, password)
+			VALUES ('$user','$pass')";
+		rec_create($conn,$sql);
+	}
+} else{
+	if (!is_null($_GET['username']) || !is_null($_GET['pass'])) {
+		echo "Username and Password send by Post method";
+	} else {
+		$sql = "INSERT INTO APP (app_name)
+			VALUES ('$appname')";
+		rec_create($conn,$sql);
+	}
+}
+function rec_create($conn,$sql){
+	if (mysqli_query($conn, $sql)) {
+    	echo "New record created successfully";
+	} else {
+    	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
 }
 
 mysqli_close($conn);
