b63b2c4144924531d5ba77e65bea223722717c7d
Karen <karkochart@gmail.com>
2015-03-02 13:31:51 +0400

    Update DB and queries in SQL homework

--- sql/homework/Karen/Comp_Shop/database/queries.01.03.15.sql
+++
@@ -1,6 +0,0 @@
-/*
-Get sale by firms DESC
-*/
-CREATE TEMPORARY TABLE IF NOT EXISTS counts_tmp SELECT firm_id, count(firm_id) AS counts FROM History GROUP BY firm_id ORDER BY counts DESC;
-SELECT * FROM counts_tmp INNER JOIN Firms ON Firms.id = counts_tmp.firm_id;
-DROP TABLE counts_tmp;
