9ecdb3bc392478ca10fff32a37a4a212b8573c9c
Edvard-Vahan <youu@exxample.com>
2015-03-02 17:30:37 +0400

    first Java work

---
+++ Java/work/Edvard/PrintLine.java
@@ -0,0 +1,10 @@
+public class PrintLine {
+    public static void printLine(int point) {
+    
+        for (int i = 1; i <= point; i++) {
+            System.out.print('*');
+        }
+        System.out.println();
+    }
+}
+
