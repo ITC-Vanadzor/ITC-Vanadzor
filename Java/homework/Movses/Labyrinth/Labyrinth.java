c6064d531e31fe58c3c0c35363e1f239b60b2910
stepan_sargis <stepan@sargis.com>
2015-03-03 18:03:52 +0400

    Modified java labyrinth

--- Java/homework/Movses/Labyrinth/Labyrinth.java
+++ Java/homework/Movses/Labyrinth/Labyrinth.java
@@ -1,16 +1,20 @@
 public class Labyrinth {
-    public static final int N = 4;
+    public static final int N = 5;
     public static void main(String[] args) {
-        String [][] m = {
-            { ".", "#", ".", "." },
-            { ".", "#", ".", "#" },
-            { ".", ".", ".", "#" },
-            { "#", "#", ".", "." }
+        String [][] matrix = {
+            { "S", "#", "#", ".", "F" },
+            { ".", "#", ".", ".", "#" },
+            { ".", ".", "#", ".", "#" },
+            { ".", ".", ".", ".", "#" },
+            { "#", "#", ".", "#", "." }
         };
-        int i, j;
+        int i; 
+        int j;
 
         for(i=0; i<N; i++) {
             for(j=0; j<N; j++)
-                System.out.print(m[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
             System.out.println();
         }
+    }
+}
