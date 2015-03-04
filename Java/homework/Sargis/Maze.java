8d3c1e87c7cc092082ed8972d4e5aa53c80c1ccb
Sargis <sargiskocharyan@yahoo.com>
2015-03-04 10:53:18 +0400

    Author: Sargis
    Homework. Maze(labirinth) was added, plus script for easy compiling and running.
    Useless files were deleted.

--- Java/homework/Sargis/Maze.java
+++ Java/homework/Sargis/Maze.java
@@ -1,6 +1,111 @@
+import java.util.Random;
+import java.lang.Object;
 
+public class Maze {
+    int[][] maze;
+    public Maze(){
+        maze = new int[10][10];
+        for(int i=0;i<maze.length;i++){
+            for(int j=0; j<maze.length; j++){
+                maze[i][j] = -2;
+            }
+        }
+        Random rand =new Random();
+        for (int k=0; k<maze.length; ++k){
+            int a = rand.nextInt(maze.length);
+            int b = rand.nextInt(maze.length);        
+            maze[a][b] = -1;
+            a = rand.nextInt(maze.length);
+            b = rand.nextInt(maze.length);        
+            maze[a][b] = -1;
+        }
+    }
+    public void setElement(int i, int j, int value){ maze[i][j]=value;}
 
-public class Maze 
-{
+    public void printMaze(){
+        for(int i=0;i<maze.length;i++){
+            System.out.print("   |");
+            for(int j=0; j<maze[0].length; j++){
+                if (maze[i][j]!=-1){
+                    System.out.print(" "+"|");
+                }
+                else{
+                    System.out.print("H"+"|");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public void printMazeAfter(){
+        for(int i=0;i<maze.length;i++){
+            System.out.print("   |");
+            for(int j=0; j<maze[0].length; j++){
+                if (maze[i][j] !=-1 && (maze[i][j] ==-2)){
+                    System.out.print(" |");
+                }
+                else if (maze[i][j] ==-1 ){
+                    System.out.print("H"+"|");
+                }
+                else {
+                    System.out.print(maze[i][j]+"|");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public int[][] getMaze(){return maze;}
     
+    private  boolean hasFreeNeighbour(Coord counter){
+        int j=counter.getY();//right and left directions
+        int i=counter.getX();//up and down directions
+        if (i+1<10 && maze[i+1][j] == -2){
+            return true;
+        }else if((i-1) > -1 && maze[i-1][j] == -2){
+            return true;
+        }else if ((j+1)<10 && maze[i][j+1] == -2){
+            return true;
+        }else if ((j-1)>-1 && maze[i][j-1] == -2){
+            return true;
+        }
+        return false;
+    }
+
+    public int findShortPath(Coord start, Coord end,int level){
+        int i = start.getX();
+        int j = start.getY();
+        if(maze[end.getX()][end.getY()] > 0 ){
+            return maze[end.getX()][end.getY()];
+        }
+       // else if(end.getLevel() == -2 && (!hasFreeNeighbour(start))){
+         //  return 0;
+       // }
+        else{
+            if (i+1<10 && maze[i+1][j] == -2){
+                level +=1;
+                maze[i+1][j]=level;
+                Coord newStart = new Coord(i+1, j, level);
+                return findShortPath(newStart,end, level);
+            }else if((i-1) > -1 && maze[i-1][j] == -2){
+                level +=1;
+                maze[i-1][j]=level;
+                Coord newStart = new Coord(i-1, j, level);
+                return findShortPath(newStart,end, level);
+            }else if ((j+1)<10 && maze[i][j+1] == -2){
+                level +=1;
+                maze[i][j+1]=level;
+                Coord newStart = new Coord(i, j+1, level);
+                return findShortPath(newStart,end, level);
+            }else if ((j-1)>-1 && maze[i][j-1] == -2){
+                level +=1;
+                maze[i][j-1]=level;
+                Coord newStart = new Coord(i, j-1, level);
+                return findShortPath(newStart,end, level);
+            }
+            else {
+                System.out.println("unpredicted error, should be handled ");
+                return 0;
+            }
+        }         
+    }
 }
+
