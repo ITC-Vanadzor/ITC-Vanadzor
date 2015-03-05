2271db9a29a24e4ad4f6eca9d5b5d3b0c8f81c8e
Hrach <Hrach.M.96@mail.ru>
2015-03-06 01:38:47 +0400

    Add Autoservice homework

---
+++ Java/homework/Hrach/Auto_service/Car.java
@@ -0,0 +1,14 @@
+class Car {
+	private String model;
+	private String defect;
+	Car (String m,String d){
+		this.model=m;
+		this.defect=d;
+	}
+	public String getModel(){
+		return model;
+	}
+	public String getDefect(){
+		return defect;
+	}
+}
