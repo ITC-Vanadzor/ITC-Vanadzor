f4bbd2802f5a222c3ede23b4feba6d4d0f8a265b
Vahan <v_levonyan@yahoo.com>
2015-03-05 08:41:35 +0400

    Author:Vahan -Deleted Labirinth homework

--- Java/homework/Vahan/Labirynth/model/Start.java
+++
@@ -1,9 +0,0 @@
-public class Start {
-
-        public static void main(String []args) {
-            TestDijkstraAlgorithm tester = new TestDijkstraAlgorithm();
-
-            tester.testExecute();
-                       }
-
-} 
