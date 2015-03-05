f30ae89f1f6112c3a736a78ba707104347763fa2
marine_grigoryan <marishok2890@mail.ru>
2015-03-06 02:07:54 +0400

    Added Autoservice homework

---
+++ Java/homework/Marine/Autoservice/AutoService.java
@@ -0,0 +1,66 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Autoservice {
+    ArrayList<Car> history = new ArrayList<Car>();
+
+    public void addRepairHistory(Car data) {
+        history.add(data);
+    }
+
+    public int getModelCount(String mod) {
+        int index = 0;
+        for (Car carhistory : history) {
+            if (mod == carhistory.model) {
+                index++;
+            }
+        }
+        return index;
+    }
+
+    public int getDefectCount(String def) {
+        int index = 0;
+        for (Car carhistory : history) {
+            if (def == carhistory.defect) {
+                index++;
+            }
+        }
+        return index;
+    }
+
+    public String getMaxRepairDefect() {
+        Map<String, Integer> defectlist = new HashMap<String, Integer>;
+        int max = 0;
+        String maxdef = "";
+        for (Car carhistory : history) {
+            defectlist.put(carhistory.defect, getDefectCount(carhistory.defect));
+        }
+        for (Map.Entry<String, Integer> entry : defectlist.entrySet()) {
+            if (max < entry.getValue()) {
+                maxdef = entry.getKey();
+                max = entry.getValue();
+            }
+
+        }
+        return maxdef;
+    }
+    public String getMaxRepairModel() {
+        Map<String, Integer> modellist = new HashMap<String, Integer>;
+        int max = 0;
+        String maxmodel = "";
+        for (Car carhistory : history) {
+            modellist.put(carhistory.model, getModelCount(carhistory.model));
+        }
+        for (Map.Entry<String, Integer> entry : modellist.entrySet()) {
+            if (max < entry.getValue()) {
+                maxmodel = entry.getKey();
+                max = entry.getValue();
+            }
+        }
+        return maxmodel;
+
+    }
+}
+
---
+++ Java/homework/Marine/Autoservice/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+
+    String model;
+    String defect;
+
+    Car(String m, String d) {
+        this.model = m;
+        this.defect = d;
+    }
+    
+}
+
