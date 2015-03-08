ec2aec8fa75298c8d8b2aa0aaf3299773c64093a
Movses <xmovses12@mail.ru>
2015-03-09 01:55:57 +0400

    Author: Movses
        added homework triangle

---
+++ java/homework/Movses/Triangle/Erankyun.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Erankyun {
+    
+    private Point A;
+    private Point B;
+    private Point C;
+
+    public Erankyun() {
+        A = new Point(1,1);
+        B = new Point(2,2);
+        C = new Point(2,0);    
+    }   
+}
