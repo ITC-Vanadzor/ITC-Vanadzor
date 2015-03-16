d8e219eb233b5acdae92c8628b2ab37622e12e2a
Movses <xmovses12@mail.ru>
2015-03-17 01:49:27 +0400

    Update

--- java/work/Lunch/frontend/js/distributors.js
+++ java/work/Lunch/frontend/js/distributors.js
@@ -1,4 +1,4 @@
-var product = [{"product": "Iqibir", "count": 3},
+var product = [ {"product": "Iqibir", "count": 3},
     {"product": "Qyabab", "count": 2},
     {"product": "Pizza", "count": 4}
 ];
@@ -13,5 +13,15 @@ function changeName() {
 }
 
 function addProductList() {
-    
+    alert(product);
+    for(i=0; i<2; i++) {
+        document.getElementById("productTable").innerHTML = 
+        "<tr>" + "<td>" + product[i].product + "<td>" + "<td>" + product[i].count + "<td>" + "<tr>"               
+    }   
+}
+function addOrderList() {
+    for(i=0; i<order.length; i++) {
+        document.getElementById("orderTable").innerHTML = 
+        "<tr>" + "<td>" + order[1].name + "<td>" + "<td>" + order[i].product + "<td>" + "<td>" + order[i].count + "<td>" + "<tr>";               
+    }   
 }
