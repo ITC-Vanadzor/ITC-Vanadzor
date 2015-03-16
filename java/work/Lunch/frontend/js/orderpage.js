2feb8fee2ee0fcc930bcc4d54261325b4039fe1d
Hrach, Movses <Hrach.M.96@mail.ru, xmovses12@mail.ru>
2015-03-16 18:07:51 +0400

    Author: Sargis
    some improvements were made

--- java/work/Lunch/frontend/js/orderpage.js
+++ java/work/Lunch/frontend/js/orderpage.js
@@ -4,3 +4,13 @@ function loadproducts(){
 function getPlaces (){
     document.getElementById("places").innerHTML = "aaa";
 }
+function printOldOrders(){
+    var orders = '{"order_id":['+'{"place_id":"Tashir","product_id":"Pizza","count_id":"2"}]}';
+    var obj = JSON.parse(orders);
+    var node = document.createElement("div");
+    var texnode = obj.order_id[0].place_id;
+    document.getElementById("table").appendChild(node);
+    //document.getElementById("table_part2").innerHTML = obj.order_id[0].product_id;
+    //document.getElementById("table_part3").innerHTML = obj.order_id[0].count_id;
+}
+
